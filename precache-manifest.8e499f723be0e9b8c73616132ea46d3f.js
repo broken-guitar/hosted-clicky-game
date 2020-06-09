942ffcb25b9ccc45db0da72f77059d8cc1952887
David Flores <David.Flores@suesd.k12.ca.us>
2020-06-09 16:17:35 -0700

    fixed homepage path in package.json

---
+++ precache-manifest.8e499f723be0e9b8c73616132ea46d3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb46c350ab09794dfd62a1d160ab0d79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f99c1a4b52eac0a6a542",
+    "url": "./static/css/main.083e1309.chunk.css"
+  },
+  {
+    "revision": "2d35eaf71eb79c2b1290",
+    "url": "./static/js/2.9033ef57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9033ef57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99c1a4b52eac0a6a542",
+    "url": "./static/js/main.f7064b7d.chunk.js"
+  },
+  {
+    "revision": "2b6ab7e78f9297114b7f",
+    "url": "./static/js/runtime-main.c22446bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
